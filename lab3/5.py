f4e046e26cc048dba15aae8a67bb8cc9024f993b
w67709 <erykoficjalnie@interia.pl>
2022-10-25 13:13:24 +0200

    zadania

---
+++ lab3/5.py
@@ -0,0 +1,12 @@
+#Grupa laboratoryjna składa się z n studentów (wartość n podaje użytkownik). Wprowadzamy
+#liczbę punktów dla każdego studenta. Napisz program, który obliczy średnią liczbę punktów w grupie z
+#wykorzystaniem pętli while.
+
+
+n = int(input("Podaj n:3 "))
+while: (a+b+c)
+    if(a=20)
+        (b=20)
+        (c=20)
+
+
